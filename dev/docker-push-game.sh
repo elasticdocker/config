5cc0c5c17f9583654e8fbf8fc5f96994251e1e89
Manu <Newuser7>
2017-07-01 15:31:48 -0600

    docker squash

--- dev/docker-push-game.sh
+++ dev/docker-push-game.sh
@@ -1,2 +1,3 @@
+npm --prefix game-service/ run build
 docker build -t manishsingla/game-service:02 game-service/
 docker push manishsingla/game-service:02
--- dev/docker-push-word.sh
+++ dev/docker-push-word.sh
@@ -1,2 +1,3 @@
-docker build -t manishsingla/word-service:02 word-service/
+npm --prefix word-service/ run build
+docker build -t manishsingla/word-service:02 --compress --squash word-service/
 docker push manishsingla/word-service:02

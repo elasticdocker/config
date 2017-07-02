b36fbea308bb72d06554995a0252d379b0a98add
Manu <Newuser7>
2017-07-01 22:37:06 -0600

    docker squash

--- dev/docker-push-word.sh
+++ dev/docker-push-word.sh
@@ -1,3 +1,3 @@
 npm --prefix word-service/ run build
-docker build -t manishsingla/word-service:02 --compress --squash word-service/
+docker build -t manishsingla/word-service:02 --compress  word-service/
 docker push manishsingla/word-service:02

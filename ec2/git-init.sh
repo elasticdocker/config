9515e73ecfec327bca97c667e07c2bba8fe90c6b
elasticdocker <ms349293@ohio.edu>
2017-07-09 16:37:29 -0600

    change git

--- ec2/git-init.sh
+++ ec2/git-init.sh
@@ -1,6 +1,6 @@
 # TODO: remove prompt for download
 sudo yum install git
-git clone https://github.com/git4lyfe/config.git
+git clone https://github.com/elasticdocker/config.git
 sudo chmod 740 config/ec2/*.sh
 
 
